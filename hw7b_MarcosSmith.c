06c83a88661513ef927ea30ed43521175b6597d8
smitm237 <127427436+smitm237@users.noreply.github.com>
2023-04-14 13:38:22 -0700

    Done, figure out spacing

--- hw7b_MarcosSmith.c
+++ hw7b_MarcosSmith.c
@@ -6,22 +6,23 @@ int main(){
   char code[30];
   int i;
   int shift;
-  printf("Enter the Encrypted Code: ");
-  fgets(code, 30, stdin);
+  printf("Enter the Encrypted Code (STOP to quit): ");
+  scanf("%s", &code);
   int length = strlen(code);
 
-  while(strcmp(code, "STOP\n") != 0){
+  while(strcmp(code, "STOP") != 0){
     for(shift = -5; shift <= 5; shift++){
-      printf("\nFor a shift of   %d, the decyrpted code is: ", shift);
+      printf("For a shift of   %d, the decyrpted code is: ", shift);
       for(i = 0; i < length; i++){
         char c = code[i] + shift;
         printf("%c", c);
       }
+      printf("\n");
     } 
     printf("\n");
-    printf("Enter the Encrypted Code: ");
-    fgets(code, 30, stdin);
-    int length = strlen(code); 
+    printf("Enter the Encrypted Code (STOP to quit): ");
+    scanf("%s", &code);
+    length = strlen(code); 
   }
   printf("\n");
 return 0;
